252c8d4331137d9f9d3413ca0ef7db5eff3d7013
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2022-09-04 15:07:27 +0300

    remove extra transforms

--- classification/datasets.py
+++ classification/datasets.py
@@ -88,26 +88,7 @@ def build_dataset(is_train, args):
 
 def build_transform(is_train, args):
     resize_im = args.input_size > 32
-    '''
-    if is_train:
-        # this should always dispatch to transforms_imagenet_train
-        transform = create_transform(
-            input_size=args.input_size,
-            is_training=True,
-            color_jitter=args.color_jitter,
-            auto_augment=args.aa,
-            interpolation=args.train_interpolation,
-            re_prob=args.reprob,
-            re_mode=args.remode,
-            re_count=args.recount,
-        )
-        if not resize_im:
-            # replace RandomResizedCropAndInterpolation with
-            # RandomCrop
-            transform.transforms[0] = transforms.RandomCrop(
-                args.input_size, padding=4)
-        return transform
-    '''
+    
     t = []
     if resize_im:
         size = int((256 / 224) * args.input_size)
